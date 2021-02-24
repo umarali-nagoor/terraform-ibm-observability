1fed827891ad9f559a170652e2879aed06126e37
umarali-nagoor <umarali.nagoor@in.ibm.com>
2021-02-24 16:03:27 +0530

    v13_upgrade: Added required_version parameter

--- examples/sysdig-downtime-alert/versions.tf
+++ examples/sysdig-downtime-alert/versions.tf
@@ -3,7 +3,29 @@
 # Copyright 2020 IBM
 ##########################################################
 
+/***************************************************
+NOTE: To source a particular version of IBM terraform provider version, configure the parameter `version`.
+
 terraform {
+  required_version = ">=0.13"
+  required_providers {
+    sysdig = {
+      source = "sysdiglabs/sysdig"
+      version = "0.5.10"
+    }
+
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.21.0"
+    }
+  }
+}
+ 
+If we dont configure the version parameter, it fetched latest provider version.
+****************************************************/
+
+terraform {
+  required_version = ">=0.13"
   required_providers {
     sysdig = {
       source = "sysdiglabs/sysdig"
--- examples/sysdig-instance/versions.tf
+++ examples/sysdig-instance/versions.tf
@@ -5,7 +5,9 @@
 
 /***************************************************
 NOTE: To source a particular version of IBM terraform provider version, configure the parameter `version`.
- 
+
+terraform {
+  required_version = ">=0.13"
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
@@ -17,6 +19,7 @@ If we dont configure the version parameter, it fetched latest provider version.
 ****************************************************/
 
 terraform {
+  required_version = ">=0.13"
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
